124648ef26031e04242f1ac9aaadc7e30a3b1d5f
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-29 12:45:07 -0400

    feat(ast): create the basis ast for the lang

--- src/Language/PyIsh/IR/AST.hs
+++ src/Language/PyIsh/IR/AST.hs
@@ -1,3 +1,100 @@
 module Language.PyIsh.IR.AST (
-
+    Identifier,
+    Atom (..),
+    Expr (..),
+    Statement (..),
+    PrimUnaryOp (..),
+    PrimBinOp (..),
+    showAtom,
+    showPrimUnaryOp,
+    showPrimBinOp,
+    showExpr,
 ) where
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import qualified Data.Vector as Vector
+
+type Identifier = Text.Text
+
+data Atom
+    = AInt Integer
+    | AFloat Double
+    | AString Text.Text
+    | AIdent Identifier
+    deriving (Show, Read, Eq, Ord)
+
+showAtom :: Atom -> Text.Text
+showAtom = \case
+    AInt i -> Text.pack $ show i
+    AFloat f -> Text.pack $ show f
+    AString s -> s
+    AIdent i -> i
+
+data PrimUnaryOp
+    = Negate
+    deriving (Show, Read, Eq, Ord)
+
+showPrimUnaryOp :: PrimUnaryOp -> Text.Text
+showPrimUnaryOp = \case
+    Negate -> "-"
+
+data PrimBinOp
+    = -- math
+      Add
+    | Sub
+    | Mul
+    | Div
+    | -- bools - compares
+      Eq
+    | NotEq
+    | Less
+    | LessEq
+    | Greater
+    | GreaterEq
+    | -- bools - combines
+      And
+    | Or
+    | Xor
+    deriving (Show, Read, Eq, Ord)
+
+showPrimBinOp :: PrimBinOp -> Text.Text
+showPrimBinOp = \case
+    Add -> "+"
+    Sub -> "-"
+    Mul -> "*"
+    Div -> "/"
+    Eq -> "=="
+    NotEq -> "!="
+    Less -> "<"
+    LessEq -> "<="
+    Greater -> ">"
+    GreaterEq -> ">="
+    And -> "and"
+    Or -> "or"
+    Xor -> "xor"
+
+data Expr a
+    = EAtom a Atom
+    | EPrimUnaryOp a PrimUnaryOp (Expr a)
+    | EPrimBinOp a PrimBinOp (Expr a) (Expr a)
+    | ETuple a (Vector.Vector (Expr a))
+    | EList a (Vector.Vector (Expr a))
+    | EDict a (Map.Map (Expr a) (Expr a))
+    | ESet a (Set.Set (Expr a))
+    deriving (Show, Read, Eq, Ord)
+
+showExpr :: Expr a -> Text.Text
+showExpr = \case
+    EAtom _ a -> showAtom a
+    EPrimUnaryOp _ op e -> Text.concat ["(", showPrimUnaryOp op, showExpr e, ")"]
+    EPrimBinOp _ op e1 e2 -> Text.concat ["(", showExpr e1, showPrimBinOp op, showExpr e2, ")"]
+    ETuple _ xs -> undefined
+    EList _ xs -> undefined
+    EDict _ xs -> undefined
+    ESet _ xs -> undefined
+
+data Statement a
+    = Define a Identifier [Identifier] (Expr a)
+    deriving (Show, Read, Eq, Ord)
