fb14aec0b8472c18045086b81c9073584872be0a
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-29 18:40:59 -0400

    feat(ast): add a method to show statements

--- src/Language/PyIsh/IR/AST.hs
+++ src/Language/PyIsh/IR/AST.hs
@@ -9,8 +9,13 @@ module Language.PyIsh.IR.AST (
     showPrimUnaryOp,
     showPrimBinOp,
     showExpr,
+    showStatement,
 ) where
 
+import Control.Arrow (Arrow (..))
+import Control.Monad (join)
+import Data.Functor ((<&>))
+import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import qualified Data.Text as Text
@@ -21,6 +26,7 @@ type Identifier = Text.Text
 data Atom
     = AInt Integer
     | AFloat Double
+    | ABool Bool
     | AString Text.Text
     | AIdent Identifier
     deriving (Show, Read, Eq, Ord)
@@ -29,6 +35,7 @@ showAtom :: Atom -> Text.Text
 showAtom = \case
     AInt i -> Text.pack $ show i
     AFloat f -> Text.pack $ show f
+    ABool b -> if b then "true" else "false"
     AString s -> s
     AIdent i -> i
 
@@ -107,5 +114,41 @@ showExpr = \case
 
 data Statement a
     = SDefine a Identifier [Identifier] [Statement a]
+    | SIf a (Expr a, [Statement a]) [(Expr a, [Statement a])] (Maybe [Statement a])
     | SExpr a (Expr a)
     deriving (Show, Read, Eq, Ord)
+
+showStatement :: Statement a -> Text.Text
+showStatement = join showStatement' 4
+
+showStatement' :: Int -> Int -> Statement a -> Text.Text
+showStatement' step =
+    go
+  where
+    go :: Int -> Statement a -> Text.Text
+    go n = \case
+        SDefine _ f ns bs ->
+            Text.concat
+                [ top "def " (f <> "(" <> Text.intercalate ", " ns <> ")")
+                , showBody bs
+                ]
+        SIf _ (iff, ifb) elifs elseb ->
+            let if_ = top "if " (showExpr iff) <> showBody ifb
+                elif_ :: Text.Text
+                elif_ =
+                    Text.concat $
+                        (\(f, b) -> top "elif " (showExpr f) <> showBody b)
+                            <$> elifs
+                else_ = maybe "" ((top "else" "" <>) . showBody) elseb
+             in Text.intercalate (newline' (n - step)) $ [if_, elif_, else_]
+        SExpr _ e -> showExpr e
+      where
+        top :: Text.Text -> Text.Text -> Text.Text
+        top prefix inner = prefix <> inner <> ":" <> newline
+
+        next = go (n + step)
+
+        newline = newline' n
+        newline' x = Text.pack $ '\n' : (replicate x ' ')
+
+        showBody xs = Text.intercalate newline $ go (n + step) <$> xs
